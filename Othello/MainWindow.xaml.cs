721d845606431a43e50fe567534df441169f003b
dominiqu.ducommun <dominique.ducommun@he-arc.ch>
2017-01-20 09:22:15 +0100

    Ajout de l'interaction avec le board, des images de pions et utilisation de grid

--- Othello/MainWindow.xaml.cs
+++ Othello/MainWindow.xaml.cs
@@ -20,16 +20,19 @@ namespace Othello
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Gameboard gb;
+
         public MainWindow()
         {
             InitializeComponent();
             Console.WriteLine("Bonjour : "+(char) 97);
+            
+            this.gb = new Gameboard();
 
-            Gameboard gb = new Gameboard();
+            this.gb.drawBoard();
 
-            gb.drawBoard();
-
-            if (gb.isPlayable(3, 2, true))
+            if (this.gb.isPlayable(3, 2, true))
             {
                 Console.WriteLine("OUIOUI");
             }
@@ -39,55 +42,70 @@ namespace Othello
             {
                 for(int j = 0; j < 8; j++)
                 {
-                    if (gb.isPlayable(i, j, false))
+                    if (this.gb.isPlayable(i, j, false))
                     {
                         Console.WriteLine("("+i+", "+j+")");
                     }
                 }
             }
 
-            // Les 2 permettent de tester l'ajout en colonne dans les 2 sens.
-            //gb.playMove(3, 2, true);
-            //gb.playMove(4, 5, true);
 
-            // Les 2 permettent de tester l'ajout en ligne dans les 2 sens.
-            //gb.playMove(2, 3, true);
-            //gb.playMove(5, 4, true);
 
-            //Les 2 permettent de tester l'ajout en colonne dans les 2 sens.
-            //gb.playMove(5, 2, true);
-            //gb.playMove(2, 5, true);
-            //gb.playMove(5, 5, false);
-            //gb.playMove(2, 2, false);
+            this.gb.drawBoard();
 
-            gb.drawBoard();
+            // Initialisation de la grille
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+
+                    CaseUserControl control = new CaseUserControl(i, j);
+
+                    // Enregistrement des événements
+                    control.MouseLeftButtonUp += MouseLeftButtonUpCase;
+                    control.MouseLeave += MouseLeaveCase;
+                    control.MouseEnter += MouseEnterCase;
+
+                    control.pawnBorder.Background = (i % 2 == 0) ? (j % 2 == 0) ? Brushes.ForestGreen : Brushes.DarkGreen : (j % 2 == 0) ? Brushes.DarkGreen : Brushes.ForestGreen;
+
+                    Grid.SetColumn(control, i);
+                    Grid.SetRow(control, j);
+
+                    BoardGrid.Children.Add(control);
+
+
+
+                }
+            }
 
             
 
-            CheckerBoard.Rows = 8;
-            CheckerBoard.Columns = 8;
+            
 
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Rectangle rectVert = new Rectangle();
-                    rectVert.Fill = Brushes.ForestGreen;
-                    Rectangle rectDark = new Rectangle();
-                    rectDark.Fill = Brushes.DarkGreen;
-                    if (i % 2 == 0)
-                    {
-                        CheckerBoard.Children.Add(rectDark);
-                        CheckerBoard.Children.Add(rectVert);
-                    }
-                    else
-                    {
-                        CheckerBoard.Children.Add(rectVert);
-                        CheckerBoard.Children.Add(rectDark);
-                    }
-                }
-
-            }
         }
+
+        private void MouseLeftButtonUpCase(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("CLICKED");
+            CaseUserControl control = (CaseUserControl) sender;
+            //CaseUserControl.empty = false;
+            // TESTER QUI JOUE POUR COULEUR if(this.gb.isPlayable(control.X, control.X, ))
+            // PLAYMOVE
+            control.pawnImage.Source = new BitmapImage(new Uri(@"img/white.png", UriKind.Relative));
+        }
+
+        private void MouseEnterCase(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("ENTER");
+        }
+
+        private void MouseLeaveCase(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Leave");
+        }
+
+
+
+
     }
 }
